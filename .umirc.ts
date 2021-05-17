744684c33a51176b3f822f2681920ee0f5bb4ead
zkboys <876213774@qq.com>
2021-05-17 18:50:42 +0800

    :pencil: 整理文档

--- .umirc.ts
+++ .umirc.ts
@@ -7,7 +7,7 @@ export default defineConfig({
         'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
     logo:
         'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-    outputPath: 'docs-dist',
+    outputPath: 'docs',
     mode: 'site',
     alias: {
         '@ife/pro-components': resolve(
@@ -18,7 +18,7 @@ export default defineConfig({
     },
     resolve: {
         includes: [
-            'docs',
+            'docs-src',
             'packages',
         ],
     },
