048934aa9f1548dc547c6df9c39e3ba97d3d4196
闻佳 <wen_j@suixingpay.com>
2021-06-12 15:19:57 +0800

    首次无法build问题

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,7 +1,23 @@
+import path from 'path';
+import fs from 'fs';
+
+const packagesPath = path.join(__dirname, 'packages');
+let packageNames = fs.readdirSync(packagesPath).filter(item => !item.startsWith('.'));
+const pkgs = [
+    'util',
+    'hooks',
+];
+packageNames = packageNames.filter(name => !pkgs.includes(name));
+
+
 export default {
     cjs: { type: 'babel', lazy: true },
     esm: {
         type: 'babel',
         importLibToEs: true,
     },
+    pkgs: [
+        ...pkgs,
+        ...packageNames,
+    ],
 };
