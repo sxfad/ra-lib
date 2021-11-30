96380af596caa865eb7eed2b8e5ad3bc53cbe637
zkboys <876213774@qq.com>
2021-11-30 15:06:10 +0800

    :sparkles: 添加新功能

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,4 @@
 export { default as useTest } from './useTest';
 export { default as useHeight } from './useHeight';
 export { default as useDebounceValidator } from './useDebounceValidator';
+export { default as useQueryState } from './useQueryState';
