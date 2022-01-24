c91b3f6c0744124ab23d634ba3e48965e83fcb09
zkboys <876213774@qq.com>
2022-01-24 17:36:49 +0800

    :sparkles: 导出类型

--- packages/ajax/src/index.ts
+++ packages/ajax/src/index.ts
@@ -4,4 +4,6 @@ import Ajax from './Ajax';
 export { default as createHoc } from './create-hoc';
 export { default as createHooks } from './create-hooks';
 
+export * from './Ajax';
+
 export default Ajax;
