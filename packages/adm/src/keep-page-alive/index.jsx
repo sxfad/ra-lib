f8628e2f85b202400c7c5869d9e4663b6ec91b72
zkboys <876213774@qq.com>
2022-03-31 18:44:08 +0800

    :sparkles: 添加新功能

--- packages/adm/src/keep-page-alive/index.jsx
+++ packages/adm/src/keep-page-alive/index.jsx
@@ -1,6 +1,6 @@
-import React, {useMemo, useRef, useState, useEffect, forwardRef, useImperativeHandle} from 'react';
-import {match} from 'path-to-regexp';
-import {Route, Routes, useLocation} from 'react-router-dom';
+import React, { useMemo, useRef, useState, useEffect, forwardRef, useImperativeHandle } from 'react';
+import { match } from 'path-to-regexp';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 function KeepPageAlive(props, ref) {
     const { routes, error404, hashRouter, baseName, ejectProps } = props;
@@ -44,37 +44,34 @@ function KeepPageAlive(props, ref) {
         if (!keepRoutes.length) return;
         const key = hashRouter ? hash.replace('#', '') : `${pathname}${search}${hash}`;
 
-        let pageIndex = -1;
-        keepPagesRef.current.forEach((item, i) => {
-            // eslint-disable-next-line no-param-reassign
-            item.active = false;
-            if (item.key === key) pageIndex = i;
-        });
-
         // 保持页面的路由
         let keepRoute = keepRoutes.find(({ path }) => match(path, { decode: decodeURIComponent })(routePath));
         // 非保持页面路由
         const unKeepRoute = unKeepRoutes.find(({ path }) => match(path, { decode: decodeURIComponent })(routePath));
 
-        // 未找到已存在页面，添加一个
-        if (pageIndex === -1 && keepRoute) {
-            keepPagesRef.current.unshift({
+        // 所有先标记为非激活
+        keepPagesRef.current.forEach(item => {
+            // eslint-disable-next-line no-param-reassign
+            item.active = false;
+        });
+
+        let nextPage = keepPagesRef.current.find(item => item.key === key);
+
+        // 当前页面需要保持，但是没有在keepPagesRef.current中
+        if (keepRoute && !nextPage) {
+            nextPage = {
                 path: keepRoute?.path,
                 key,
                 Component: keepRoute?.Component,
-            });
+            };
+            keepPagesRef.current.unshift(nextPage);
         }
 
-        if (pageIndex !== -1) {
-            // 页面已存在，当前页面标记激活
-            const nextPage = keepPagesRef.current[pageIndex];
-
-            // active 初次加载为：undefined，隐藏为：false，再次激活为：true
-            nextPage.active = true;
-        }
+        // 标记为激活
+        if (nextPage) nextPage.active = true;
 
         // 路由不存在，渲染 404 页面
-        if (!keepRoute && !unKeepRoute) {
+        if (!keepRoute && !unKeepRoute && !nextPage) {
             keepPagesRef.current.unshift({
                 path: routePath,
                 key,
@@ -116,7 +113,7 @@ function KeepPageAlive(props, ref) {
 
                 return (
                     <div key={key} style={{ display }}>
-                        <Component {...ejectProps} {...activeProps}/>
+                        <Component {...ejectProps} {...activeProps} />
                     </div>
                 );
             })}
@@ -125,10 +122,10 @@ function KeepPageAlive(props, ref) {
             <Routes>
                 {unKeepRoutes.map(item => {
                     const { path, Component } = item;
-                    return (<Route key={path} exact path={path} element={<Component {...ejectProps}/>}/>);
+                    return (<Route key={path} exact path={path} element={<Component {...ejectProps} />} />);
                 })}
                 {/* 防止路由找不到会报提醒 */}
-                <Route exact path="*" element={<div/>}/>
+                <Route exact path='*' element={<div />} />
             </Routes>
         </>
     );
